f5ba6ec060e5141082f612c01ba2993461d2b0e5
Jeff Stapleton <jeff.d.stapleton@gmail.com>
2015-04-30 10:53:08 -0600

    init tutorial

---
+++ Source/Tutorial/TutorialGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Tutorial.h"
+#include "TutorialGameMode.h"
+
+
+
+
---
+++ Source/Tutorial/TutorialGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "TutorialGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TUTORIAL_API ATutorialGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
